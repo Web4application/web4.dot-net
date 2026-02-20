7b999201ac714f325438e6570cd9d54f89fe99f4
Rylan Barnes <rylan@barn.es>
2026-02-20 08:38:20 -0600

    Namespace change

--- src/Keyholes/Assets/AssetsHelper.cs
+++ src/Keyholes/Assets/AssetsHelper.cs
@@ -8,13 +8,13 @@ public static class AssetsHelper
     public static readonly byte[] WEB4_JS = 
         Encoding.UTF8.GetBytes(new StreamReader(System.Reflection.Assembly
             .GetExecutingAssembly()
-            .GetManifestResourceStream("Web4.Keyholes.Assets.web4.js")!
+            .GetManifestResourceStream("Keyholes.Assets.web4.js")!
         ).ReadToEnd());
 
     public static readonly byte[] WEB4_CSS = 
         Encoding.UTF8.GetBytes(new StreamReader(System.Reflection.Assembly
             .GetExecutingAssembly()
-            .GetManifestResourceStream("Web4.Keyholes.Assets.web4.css")!
+            .GetManifestResourceStream("Keyholes.Assets.web4.css")!
         ).ReadToEnd());
 
 }
